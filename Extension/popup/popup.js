5797ebc8e9c4b539dc1ae3649cf265cd565527d9
Gus Perez <gus@gusperez.com>
2022-11-24 13:37:42 -0800

    Now get the active tab's URL

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -2,8 +2,11 @@ console.log("This is a popup!");
 
 document.getElementById("myButton").addEventListener("click", handler);
 
-function handler()
+async function handler()
 {
-    console.log("Clicked!");
-    alert("clicked");
+    let queryOptions = { active: true };
+    let tab = await chrome.tabs.query(queryOptions);
+    
+    console.log("Clicked: " + tab[0].url);
+    alert("Clicked: " + tab[0].url);
 }
