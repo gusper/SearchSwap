f4260946d6cb268e7922cd319b7e972061a5f66b
Gus Perez <gus@gusperez.com>
2022-11-24 13:01:37 -0800

    Clickable button added

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,1 +1,9 @@
 console.log("This is a popup!");
+
+document.getElementById("myButton").addEventListener("click", handler);
+
+function handler()
+{
+    console.log("Clicked!");
+    alert("clicked");
+}
