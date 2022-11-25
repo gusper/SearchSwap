e979425fbc97a01c1546a4ae1f445246ecb1c0af
Gus Perez <gus@gusperez.com>
2022-11-24 19:33:59 -0800

    Parse URL to extract user's search text

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,12 +1,33 @@
-console.log("This is a popup!");
-
+/**
+ * Register event handlers
+ */
 document.getElementById("myButton").addEventListener("click", handler);
 
+/**
+ * Click event handler
+ */
 async function handler()
 {
     let queryOptions = { active: true };
     let tab = await chrome.tabs.query(queryOptions);
+    let searchText = await getSearchText(tab[0].url, "q");
+
+    alert("q: " + searchText);
+}
+
+/**
+ * Takes a URL returns the specified argument's value.
+ */
+async function getSearchText(url, variable) {
+    let queryString = url.substring(url.indexOf("?") + 1, url.length);
+    let vars = queryString.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+        let pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
     
-    console.log("Clicked: " + tab[0].url);
-    alert("Clicked: " + tab[0].url);
+    console.log("Query variable '%s' not found", variable);
 }
