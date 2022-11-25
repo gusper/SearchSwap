9579d7d32375c0a6709d10e0a7f246ddbb89365e
Gus Perez <gus@gusperez.com>
2022-11-25 13:40:16 -0800

    First basic version (#1)
    Some things are hardcoded still, but it works enough for me to use for now.

---
+++ Extension/popup/popup.js
@@ -0,0 +1,40 @@
+const btns = [...document.getElementsByClassName("target")];
+btns.forEach(btn => {
+    btn.addEventListener("click", handler);
+});
+
+const targetList = new Map([ 
+    ["bing", "https://www.bing.com/search?q=QQQ"],
+    ["google", "https://www.google.com/search?q=QQQ"],
+    ["ultimateguitar", "https://www.ultimate-guitar.com/search.php?search_type=title&value=QQQ"],
+    ["stackoverflow", "https://stackoverflow.com/search?q=QQQ"],
+    ["reddit", "https://www.reddit.com/search/?q=QQQ"],
+    ["twitter", "https://twitter.com/search?q=QQQ"],
+    ["duckduckgo", "https://duckduckgo.com/?q=QQQ"]
+
+]);
+
+async function handler(sender)
+{
+    console.log(sender.srcElement.id);
+    let queryOptions = { active: true };
+    let tab = await chrome.tabs.query(queryOptions);
+    let searchText = await getSearchText(tab[0].url, "q");
+    if (typeof searchText === "undefined")
+        searchText = await getSearchText(tab[0].url, "value");
+    let targetKey = sender.srcElement.id.substring(3).toLowerCase();
+    let searchUrl = targetList.get(targetKey).replace("QQQ", searchText);
+    chrome.tabs.create({ url: searchUrl })
+}
+
+async function getSearchText(url, variable) {
+    let queryString = url.substring(url.indexOf("?") + 1, url.length);
+    let vars = queryString.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+        let pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # SearchSwap
-
 Minimal and super simple search engine hopper extension for Chrome and Edge.
