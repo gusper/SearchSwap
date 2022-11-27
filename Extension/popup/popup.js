efdd9d4e1524f1b4f595a739b41acd2cda64dc09
Gus Perez <gus@gusperez.com>
2022-11-27 13:21:34 -0800

    Add icons, updated styles, fonts, and more info link (#7)
    * Removed unneeded console.logs
    
    * No need for variable arg anymore (for now)
    
    * Optimize extracting search terms from current url
    
    * Added icons with logo
    
    * New icons, lots of CSS, new fonts
    
    * Show error message when no search text in current URL
    
    * Simplified smaller versions of logo
    
    * Add footer and More Info link

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,5 +1,6 @@
 const btns = [...document.getElementsByClassName("target")];
 btns.forEach(btn => { btn.addEventListener("click", handler); });
+btnMoreInfo.addEventListener("click", moreInfoHandler);
 
 const targetList = new Map([
     ["amazon", "https://www.amazon.com/s?k=%s"],
@@ -13,31 +14,41 @@ const targetList = new Map([
     ["twitter", "https://twitter.com/search?q=%s"],
     ["ultimateguitar", "https://www.ultimate-guitar.com/search.php?search_type=title&value=%s"],
     ["youtube", "https://www.youtube.com/results?search_query=%s&page={startPage?}&utm_source=opensearch"],
+    ["hackernews", "https://hn.algolia.com/?q=%s"]
 ]);
 
 async function handler(sender)
 {
-    console.log(sender.srcElement.id);
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
     let searchText = await getSearchText(tab.url);
     let targetKey = sender.srcElement.id.substring(3).toLowerCase();
+    
+    if (!searchText) {
+        alert("Not finding search text in current tab's URL.");
+        return;
+    }
+    
     let searchUrl = targetList.get(targetKey).replace("%s", searchText);
     chrome.tabs.create({ url: searchUrl })
 }
 
-async function getSearchText(url, variable) {
+async function getSearchText(url) {
     let searchIdentifiers = ["q", "value", "search_query", "k"];
     let queryString = url.substring(url.indexOf("?") + 1, url.length);
     let vars = queryString.split('&');
-    console.log(vars);
   
-    for (let siIndex = 0; siIndex < searchIdentifiers.length; siIndex++) {
-        for (var i = 0; i < vars.length; i++) {
+    for (var i = 0; i < vars.length; i++) {
+        for (let siIndex = 0; siIndex < searchIdentifiers.length; siIndex++) {
             let pair = vars[i].split('=');
             if (decodeURIComponent(pair[0]) === searchIdentifiers[siIndex]) {
                 return decodeURIComponent(pair[1]);
             }
         }
     }
+}
+
+async function moreInfoHandler(sender)
+{
+    chrome.tabs.create({ url: "https://gusperez.com/software/searchswap" })
 }
