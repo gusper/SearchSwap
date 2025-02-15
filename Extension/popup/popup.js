dc4dcbf71f6dcae313613386fe28a8b68c529cd6
Gus Perez <gus@gusperez.com>
2025-02-14 18:38:06 -0800

    Add support for Bluesky searches

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -5,16 +5,17 @@ btnMoreInfo.addEventListener("click", moreInfoHandler);
 const targetList = new Map([
     ["amazon", "https://www.amazon.com/s?k=%s"],
     ["bing", "https://www.bing.com/search?q=%s"],
+    ["bluesky", "https://bsky.app/search?q=%s"]
     ["duckduckgo", "https://duckduckgo.com/?q=%s"],
     ["github", "https://github.com/search?q=%s&ref=opensearch"],
     ["google", "https://www.google.com/search?q=%s"],
+    ["hackernews", "https://hn.algolia.com/?q=%s"]
     ["reddit", "https://www.reddit.com/search/?q=%s"],
     ["stackoverflow", "https://stackoverflow.com/search?q=%s"],
     ["superuser", "https://superuser.com/search?q=%s"],
     ["twitter", "https://twitter.com/search?q=%s"],
     ["ultimateguitar", "https://www.ultimate-guitar.com/search.php?search_type=title&value=%s"],
     ["youtube", "https://www.youtube.com/results?search_query=%s&page={startPage?}&utm_source=opensearch"],
-    ["hackernews", "https://hn.algolia.com/?q=%s"]
 ]);
 
 async function handler(sender)
