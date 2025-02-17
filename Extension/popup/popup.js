3496a803578bbada60c79a38f97728aa9ea1b521
Gus Perez <gus@gusperez.com>
2025-02-16 18:45:56 -0800

    Rename 'targets' div to 'sites' in popup.html and update related JavaScript references for improved clarity

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -13,21 +13,25 @@ chrome.storage.sync.get({ targetList: defaultTargetList }, function(data) {
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-    console.log('In onChanged listener');
     if (changes.targetList) {
         sitesMap = new Map(changes.targetList.map(item => [item.id, item]));
         initializePopup();
     }
 });
 
+/**
+ * Initializes the popup by populating the sites div with buttons for each item in the sitesMap.
+ * Clears any existing elements in the sites div before adding new ones.
+ * Adds event listeners to the created buttons and the "More Info" button.
+ */
 function initializePopup() {
-    const targetsDiv = document.getElementById('targets');
+    const targetsDiv = document.getElementById('sites');
 
     if (targetsDiv) {
         // Clear existing elements
         targetsDiv.innerHTML = '';
 
-        // Create a new element for each item in targetList
+        // Create a new element for each item in sitesMap
         sitesMap.forEach((item, name) => {
             const newElement = document.createElement('button');
             newElement.textContent = item.name;
@@ -38,15 +42,21 @@ function initializePopup() {
         });
     }
 
+    // Add event listeners to buttons
     const btns = [...document.getElementsByClassName("target")];
-    console.log('btns', btns);
-    btns.forEach(btn => { 
-        btn.addEventListener("click", handler); 
-        console.log('btn.id', btn.id);
-    });
+    btns.forEach(btn => { btn.addEventListener("click", handler); });
     btnMoreInfo.addEventListener("click", moreInfoHandler);
 }
 
+/**
+ * Handles the click event from the sender element, retrieves the current active tab's URL,
+ * extracts the search text from the URL, and opens a new tab with the search URL based on the sender's ID.
+ *
+ * @param {Object} sender - The object representing the element that triggered the event.
+ * @param {Object} sender.srcElement - The source element that triggered the event.
+ * @param {string} sender.srcElement.id - The ID of the source element.
+ * @returns {Promise<void>} - A promise that resolves when the function completes.
+ */
 async function handler(sender) {
     console.log('in handler(' + sender.srcElement.id + ')');
     let queryOptions = { active: true, currentWindow: true };
@@ -65,6 +75,12 @@ async function handler(sender) {
     chrome.tabs.create({ url: searchUrl })
 }
 
+/**
+ * Extracts the search text from a given URL based on predefined search query parameter identifiers.
+ *
+ * @param {string} url - The URL to extract the search text from.
+ * @returns {Promise<string|undefined>} - A promise that resolves to the search text if found, otherwise undefined.
+ */
 async function getSearchText(url) {
     console.log('getSearchText(' + url + ')');
     let searchIdentifiers = ["q", "value", "search_query", "k"];
@@ -81,6 +97,13 @@ async function getSearchText(url) {
     }
 }
 
+/**
+ * Handles the event when the More Info link is clicked.
+ * Opens a new tab with the SearchSwap information page.
+ *
+ * @param {Object} sender - The sender object that triggered the event.
+ * @returns {Promise<void>} - A promise that resolves when the tab is created.
+ */
 async function moreInfoHandler(sender) {
     chrome.tabs.create({ url: "https://gusperez.com/searchswap" })
 }
